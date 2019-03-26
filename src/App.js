f421fbdf2bb2db7a69234633dd1ed34abc043922
Jefferson Gomes <30440307+fergo8@users.noreply.github.com>
2019-03-26 16:05:23 -0300

    Implementada função tick()

--- src/App.js
+++ src/App.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Clock from "./components/Clock";
 
-ReactDOM.render(<Clock date={new Date()} />, document.getElementById("root"));
+ReactDOM.render(<Clock />, document.getElementById("root"));
