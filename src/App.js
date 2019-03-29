3a0057b5d590e136c9eca3a37fda981c40f10481
Jefferson Gomes <30440307+fergo8@users.noreply.github.com>
2019-03-29 13:27:10 -0300

    Imagem de fundo inserida

--- src/App.js
+++ src/App.js
@@ -2,4 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Clock from "./components/Clock";
 
+import "./App.css";
+
 ReactDOM.render(<Clock />, document.getElementById("root"));
